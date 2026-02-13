ee5c55be8e59562300b71fb208e97b59cec54e1a
OLUSANYA TIMOTHY <olusanyatimothy54@gmail.com>
2026-02-13 22:54:21 +0100

    featured session adjusted and refined

--- src/sections/FeaturesSection.tsx
+++ src/sections/FeaturesSection.tsx
@@ -2,22 +2,23 @@
 import SectionTitle from "../components/SectionTitle";
 import { ArrowUpRight } from "lucide-react";
 import { motion } from "motion/react";
+import { Link } from "react-router-dom";
 import { featuresData } from "../data/features";
 import type { IFeature } from "../types";
 
 export default function FeaturesSection() {
     return (
         <div id="features" className="px-4 md:px-16 lg:px-24 xl:px-32">
-            <SectionTitle text1="Features" text2="What you get" text3="Components, patterns and pages — everything you need to ship." />
+            <SectionTitle text1="Features" text2="What you’ll love" text3="Components, patterns and pages — everything you need to ship." />
             <div className="flex flex-wrap items-center justify-center gap-6 md:gap-4 mt-16 px-6">
                 {featuresData.map((feature: IFeature, index: number) => (
-                    <motion.div key={index} className={`${index === 1 ? 'p-px rounded-[13px] bg-linear-to-br from-pink-600 to-slate-800' : ''}`}
+                    <motion.div key={index} className={`${index === 1 ? 'p-px rounded-[13px] bg-gradient-to-br from-pink-600 to-slate-800' : ''}`}
                         initial={{ y: 150, opacity: 0 }}
                         whileInView={{ y: 0, opacity: 1 }}
                         viewport={{ once: true }}
                         transition={{ delay: index * 0.15, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                     >
-                        <div className="p-6 rounded-xl space-y-4 border border-slate-800 bg-slate-950 max-w-80 w-full">
+                        <div className="p-6 rounded-[13px] space-y-4 border border-slate-800 bg-slate-950 max-w-80 w-full">
                             {feature.icon}
                             <h3 className="text-base font-medium text-white">
                                 {feature.title}
@@ -37,7 +38,8 @@ export default function FeaturesSection() {
                     viewport={{ once: true }}
                     transition={{ type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                 >
-                    PrebuiltUI helps you build faster by transforming your design vision into fully functional, production-ready UI components.
+                    Thumblify transforms your ideas into professional LinkedIn-ready thumbnails faster, cleaner, smarter.
+
                 </motion.p>
                 <div className="grid grid-cols-1 md:grid-cols-3 mt-8 gap-10">
                     <motion.div className="md:col-span-2"
@@ -55,12 +57,12 @@ export default function FeaturesSection() {
                         transition={{ delay: 0.15, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                     >
                         <img src="/assets/features-showcase-2.png" alt="features showcase" width={1000} height={500} className="hover:-translate-y-0.5 transition duration-300" />
-                        <h3 className="text-[24px]/7.5 text-slate-300 font-medium mt-6">Better design with highest revenue and profits </h3>
-                        <p className="text-slate-300 mt-2">PrebuiltUI empowers you to build beautifully and scale effortlessly.</p>
-                        <a href="https://prebuiltui.com" className="group flex items-center gap-2 mt-4 text-pink-600 hover:text-pink-700 transition">
-                            Learn more about the product
+                        <h3 className="text-[24px]/7.5 text-slate-300 font-medium mt-6">Better design. Higher engagement. Bigger impact. </h3>
+                        <p className="text-slate-300 mt-2">Thumbnify empowers you to create stunning LinkedIn thumbnails that elevate your posts, drive engagement, and help your brand grow effortlessly.</p>
+                        <Link to="/generate" className="group flex items-center gap-2 mt-4 text-pink-600 hover:text-pink-700 transition">
+                            start generating for free
                             <ArrowUpRight className="size-5 group-hover:translate-x-0.5 transition duration-300" />
-                        </a>
+                        </Link>
                     </motion.div>
                 </div>
             </div>
