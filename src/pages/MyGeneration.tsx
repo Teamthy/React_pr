82100b7340f5611e147d52560508d7e7c4b05edd
OLUSANYA TIMOTHY <olusanyatimothy54@gmail.com>
2026-02-16 20:03:36 +0100

    Added Empty And Loading State Functionality

--- src/pages/MyGeneration.tsx
+++ src/pages/MyGeneration.tsx
@@ -7,9 +7,9 @@ const MyGeneration = () => {
   const [loading, setLoading] = useState(false);
 
   const fetchThumbnails = async () => {
-    setLoading(true);
+    /*setLoading(true);
     setThumbnails(dummyThumbnails as IThumbnail[]);
-    setLoading(false); // ✅ fixed
+    setLoading(true);*/
   };
 
   const handleDownload = (image_url: string) => {
@@ -51,6 +51,14 @@ const MyGeneration = () => {
               />
             ))}
             <p className="text-gray-400">Loading your thumbnails...</p>
+          </div>
+          ) : thumbnails.length === 0 ? (
+          // ✅ Empty state
+          <div className="flex flex-col items-center justify-center text-center py-16">
+            <p className="text-gray-400 text-lg">No thumbnails yet</p>
+            <p className="text-zinc-400 text-sm mt-2">
+              Generate your first thumbnail to see it here.
+            </p>
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
